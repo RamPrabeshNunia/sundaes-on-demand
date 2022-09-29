1bba51d2212871348396632d61bedfdceee1e827
rpnunia <rpnunia@watermarkinsights.com>
2022-09-29 23:15:59 +0530

    v1

---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+export default function SummaryForm() {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+    </span>
+  );
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+}
+
+
+// import React, {useState} from "react";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+
+// const SummaryForm = () => {
+//     const [tcChecked, setTcChecked] = useState(false);
+
+//     const checkboxLable = (
+//         <span>
+//             I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+//         </span>
+//     );
+
+//     return (
+//         <Form>
+//             <Form.Group controlId="terms-and-conditions">
+//                 <Form.Check type="checkbox" checked={tcChecked} onChange={(e) => setTcChecked(e.target.checked)} lable={checkboxLable} />
+//             </Form.Group>
+//             <Button variant="primary" type="submit" disabled={!tcChecked}>
+//                 Confirm order
+//             </Button>
+//         </Form>
+//     );
+// };
+
+// export default SummaryForm;
