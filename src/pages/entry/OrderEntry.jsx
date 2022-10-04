aea5af01b8b0c5d901d63eb2b73b5250c46cbad0
rpnunia <rpnunia@watermarkinsights.com>
2022-10-04 10:35:17 +0530

    v4

---
+++ src/pages/entry/OrderEntry.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Options from "./Options";
+
+const OrderEntry = () => {
+    return (
+        <div>
+            <Options optionType="scoops" />
+        </div>
+    );
+};
+
+export default OrderEntry;
